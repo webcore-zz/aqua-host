abe3e3f1e8a1f03be91d7da2511000c224474b3b
Mihai <mihai@webcore.ro>
2024-08-17 14:41:43 +0300

    make npm install optional

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,17 @@ set -e
 deployUrl="/usr/share/nginx/html"
 confUrl="/etc/nginx/nginx.conf"
 
-echo "Prefroming npm install..."
-npm ci
+
+while true; do
+    read -p "Do you wish to install node modules? " yn
+    case $yn in
+        [Yy]* ) echo "Prefroming npm install..."; npm ci; break;;
+        [Nn]* ) echo "Skipping npm install..."; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
 echo "Running build..."
 npm run build --omit=dev
 echo "Copy dist to $deployUrl"
