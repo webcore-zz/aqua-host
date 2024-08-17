36e5e109710ac2c0bcb18e5376986d1c37266efe
Mihai <mihai@webcore.ro>
2024-08-17 09:56:04 +0300

    add basic deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+deployUrl="/usr/share/nginx/html"
+confUrl="/etc/nginx/nginx.conf"
+
+echo "Prefroming npm install..."
+npm ci
+echo "Running build..."
+npm run build --omit=dev
+echo "Copy dist to $deployUrl"
+cp /dist/aqua-host $deployUrl
+echo "Update nginx"
+cp nginx.conf $confUrl
+echo "Restart neginx"
+systemctl reload nginx
+echo "Deployment succesfull"
