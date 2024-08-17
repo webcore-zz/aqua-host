066ffc103b73ec165dd71a7ff1b8649ebc1c5031
Mihai <mihai@webcore.ro>
2024-08-17 15:08:25 +0300

    add cleanup

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,26 @@ set -e
 deployUrl="/usr/share/nginx/html"
 confUrl="/etc/nginx/nginx.conf"
 
-
 while true; do
     read -p "Do you wish to install node modules? " yn
     case $yn in
-        [Yy]* ) echo "Prefroming npm install..."; npm ci; break;;
-        [Nn]* ) echo "Skipping npm install..."; break;;
-        * ) echo "Please answer yes or no.";;
+    [Yy]*)
+        echo "Prefroming npm install..."
+        npm ci
+        break
+        ;;
+    [Nn]*)
+        echo "Skipping npm install..."
+        break
+        ;;
+    *) echo "Please answer yes or no." ;;
     esac
 done
 
-
 echo "Running build..."
 npm run build --omit=dev
+echo "Clean up $deployUrl"
+rm -rf dist/aqua-host $deployUrl
 echo "Copy dist to $deployUrl"
 cp -r dist/aqua-host $deployUrl
 echo "Update nginx"
