846dac48514cf89f3f6ddf2263f4fbf818344e5c
Mihai <mihai@webcore.ro>
2024-08-17 15:30:40 +0300

    ome more try

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
 import { loadManifest } from "@angular-architects/module-federation";
-import { environment } from "./environments/environment";
 
-const manifest = environment.production
-  ? "mf.manifest.prod.json"
-  : "mf.manifest.json";
-
-loadManifest(`/assets/${manifest}`)
+loadManifest("/assets/mf.manifest.json")
   .catch((err) => console.error(err))
   .then((_) => import("./bootstrap"))
   .catch((err) => console.error(err));
